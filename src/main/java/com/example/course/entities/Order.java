a821f7ccde4e5061095a12847a7ee56d6e167b37
Gabriel Sousa <gabrielsousa.ti@gmail.com>
2021-02-05 11:08:52 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -74,17 +74,25 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+	
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null)
+			this.orderStatus = orderStatus.getCode();
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
-	}
-	
-	public Set<OrderItem> getItems() {
-		return items;
 	}
 
 	@Override
@@ -103,16 +111,6 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
-
-	public OrderStatus getOrderStatus() {
-		return OrderStatus.valueOf(orderStatus);
-	}
-
-	public void setOrderStatus(OrderStatus orderStatus) {
-		if(orderStatus != null)
-			this.orderStatus = orderStatus.getCode();
-	}
-	
 	
 
 }
